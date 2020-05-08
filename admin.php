22bec09d40a528376fd1e2d9c9aa120ed9051beb
James <jwrice1188@gmail.com>
2020-05-07 23:06:20 -0500

    ...

--- admin.php
+++ admin.php
@@ -1,99 +1,76 @@
 <?php
 session_start();
-if (!isset($_SESSION["fname"])){
+if (!isset($_SESSION["fname"])) {
 ?>
 
-<script> window.location.href = "login.php";</script>
+  <script>
+    window.location.href = "login.php";
+  </script>
 
-<?php } else{ 
-        if ($_SESSION["adminrights"] != 1) {
-?>
+  <?php } else {
+  if ($_SESSION["adminrights"] != 1) {
+  ?>
 
-<script> window.location.href = "index.php";</script>
+    <script>
+      window.location.href = "index.php";
+    </script>
 
 <?php }
-        $sessionfname = $_SESSION["fname"];
-        $sessionid = $_SESSION["accountid"];
-        $sessionadmin = $_SESSION["adminrights"];
+  $sessionfname = $_SESSION["fname"];
+  $sessionid = $_SESSION["accountid"];
+  $sessionadmin = $_SESSION["adminrights"];
 }
 
 $error = "";
 
 require_once("connect-db.php");
-$sql = "select * from product";
+$sql = "select * from account";
 $statement1 = $db->prepare($sql);
 if ($statement1->execute()) {
-    $products = $statement1->fetchAll();
-if ($products == null) {
-    $error = "No products entered.";
-}
-    $statement1->closeCursor();
+  $accounts = $statement1->fetchAll();
+  if ($accounts == null) {
+    $error = "No accounts have been input yet";
+  }
+  $statement1->closeCursor();
 } else {
-    $error = "Error finding products";
+  $error = "Error finding accounts";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php include("includes/adminheader.php");?>
-<!--==========================
-  Admin Section
-  ============================-->
-<section id="about">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Administrative Tasks</h3>
-                <div class="section-title-divider"></div>
-            </div>
-        </div>
+<?php include("includes/headnav.php"); ?>
+<div class="container-fluid p-0">
+  <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+    <div class="w-100">
+      <h1 class="mb-0" id="admin">ADMIN</h1><br>
+      <p class="lead mb-5">Welcome to the Admin section! Use this space to view your employee's information and alter their token count.</p>
+      <table class="table">
+        <tr>
+          <th>Employee ID</th>
+          <th>Employee Name</th>
+          <th>Tokens</th>
+        </tr>
+        <form style="text-align: center" action="updatetokens-process.php" method="POST">
+        <?php
+        if ($accounts != null) {
+          foreach ($accounts as $account) { ?>
+            <tr>
+              <td><?php echo $account["employeeid"] ?></td>
+              <td><?php echo $account["fname"] ?></td>
+              <td><input type="number" step="1" min="0" name=<?php echo $account["accountid"] ?> class="tInput" value="<?php echo $account["tokens"] ?>"></td>
+            </tr>
+        <?php }
+        } else {
+          echo "<td><h4>$error</h4></td><td></td><td></td><td></td>";
+        }
+        ?>
+      </table>
+        <input type="submit" class="btn btn-primary" value="Save Changes">
+      </form>
     </div>
-    <div class="container about-container wow fadeInUp">
-        <div class="row">
-            <table class="table table-hover">
-                <tr>
-                    <td><a href="adminpurchases.php" class="btn btn-primary" role="button">View All Purchases</a></td>
-                    <td><a href="addproduct.php" class="btn btn-primary" role="button">Add New Product</a></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Product</th>
-                    <th>Type</th>
-                    <th>Description</th>
-                    <th>Inventory</th>
-                    <th>Price</th>
-                    <th>Options</th>
-                </tr>
-                <?php 
-                    if ($products != null) {
-                    foreach ($products as $product) { ?>
-                <tr>                    
-                    <td><?php echo $product["name"]?></td>
-                    <td><?php echo $product["type"]?></td>
-                    <td><?php echo $product["description"]?></td>
-                    <td><?php echo $product["quantity"]?></td>
-                    <td><?php echo $product["price"]?></td>
-                    <td class="text-center">
-                        <form action="updateitem.php" method="POST">
-                            <input type="hidden" name="productid" value="<?php echo $product["productid"]?>">
-                            <input type="submit" class="btn btn-primary" value="Update">
-                        </form>
-                        <form action="deleteadminitem.php" method="POST">
-                            <input type="hidden" name="productid" value="<?php echo $product["productid"]?>">
-                            <input type="submit" class="btn btn-primary" value="Remove">
-                        </form>
-                    </td>
-                </tr>
-                <?php } } else {
-                    echo "<td><h4>$error</h4></td><td></td><td></td><td></td><td></td><td></td>";
-                }
-                ?>
-            </table>
-        </div>
-    </div>
-</section>
-<?php include("includes/footer.php");?>
+  </section>
+</div>
+<?php include("includes/scripts.php"); ?>
+
 </html>
--- login.php
+++ login.php
@@ -1,78 +1,90 @@
 <?php
 session_start();
-if (isset($_SESSION["fname"])){
+if (isset($_SESSION["fname"])) {
 ?>
 
-<script> window.location.href = "index.php";</script>
+  <script>
+    window.location.href = "index.php";
+  </script>
 
-<?php }
+  <?php }
 
-$cusername = $cpassword = $success = ""; 
-$error = "no error"; 
- if ($_SERVER["REQUEST_METHOD"] == "POST") { 
-    $cusername = $_POST['cusername'];
-    $cpassword = $_POST['cpassword']; 
-    require_once("connect-db.php"); 
-    $sql="select * from account where accountusername = :cusername and accountpassword = :cpassword"; 
-    $statement1=$db->prepare($sql); 
-    $statement1->bindValue(":cusername", $cusername);
-    $statement1->bindValue(":cpassword", $cpassword);
- 
-    $statement1->execute(); 
-    $result = $statement1->fetchAll();
-    $statement1->closeCursor();
-     
-    foreach($result as $theresult){ 
-        $sessionfname = $theresult['fname'];
-        $sessionid = $theresult['accountid'];
-        $sessionadmin = $theresult['adminrights'];
-    } 
-    if (isset($sessionfname)) {
-        $_SESSION["fname"] = $sessionfname; 
-        $_SESSION["accountid"] = $sessionid; 
-        $_SESSION["adminrights"] = $sessionadmin;
-?>
+$cusername = $cpassword = $success = "";
+$error = "no error";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $cusername = $_POST['cusername'];
+  $cpassword = $_POST['cpassword'];
+  require_once("connect-db.php");
+  $sql = "select * from account where accountusername = :cusername and accountpassword = :cpassword";
+  $statement1 = $db->prepare($sql);
+  $statement1->bindValue(":cusername", $cusername);
+  $statement1->bindValue(":cpassword", $cpassword);
 
-<script type="text/javascript">window.location = "index.php";</script>
+  $statement1->execute();
+  $result = $statement1->fetchAll();
+  $statement1->closeCursor();
 
-<?php 
-    }else{ 
-        $error = "Incorrect username and password"; 
-    } 
-} 
+  foreach ($result as $theresult) {
+    $sessionfname = $theresult['fname'];
+    $sessionid = $theresult['accountid'];
+    $sessionadmin = $theresult['adminrights'];
+    $sessiontokens = $theresult['tokens'];
+
+    if($sessiontokens < 1){
+      ?>
+        <script type="text/javascript">
+        window.location = "login-fail.php";
+        </script>
+      <?
+    }
+  }
+  
+  if (isset($sessionfname)) {
+    $_SESSION["fname"] = $sessionfname;
+    $_SESSION["accountid"] = $sessionid;
+    $_SESSION["adminrights"] = $sessionadmin;
+  ?>
+
+    <script type="text/javascript">
+      window.location = "index.php";
+    </script>
+
+<?php
+  } else {
+    $error = "Incorrect username and password";
+  }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php include("includes/lessheader.php");?>
-    <section id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <?php
-                if ($error != "no error") {
-                    echo "<h4 style='color: red;'>$error</h4>";
-                } else {
-                    echo $success;
-                }
-                ?>
-            <h1>Please Login</h1>
-            <h5>Don't have an account? <a href="products.php#contact">Register now!</a></h5>
-            <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+<?php include("includes/headnav.php"); ?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <?php
+      if ($error != "no error") {
+        echo "<h4 style='color: red;'>$error</h4>";
+      } else {
+        echo $success;
+      }
+      ?>
+      <h1>Please Login</h1>
+      <h5>Don't know your login information? Please talk with your supervisor.</h5>
+      <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 
-                <label>Username:</label>
-                <input class="form-control" type="text" name="cusername" required><br>
-               
-                <label>Password:</label>
-                <input class="form-control" type="password" name="cpassword" required><br>
-                
-                <input type="submit" class="btn btn-primary" name="submit" value="Submit">&nbsp;
-                <a href="index.php" class="btn btn-primary" role="button">Cancel</a>
-            </form>
-                </div>
-            </div>
-        </div>
-    </section>
-    <?php include("includes/footer.php");?>
+        <label>Username:</label>
+        <input class="form-control" type="text" name="cusername" required><br>
+
+        <label>Password:</label>
+        <input class="form-control" type="password" name="cpassword" required><br>
+
+        <input type="submit" class="btn btn-primary" name="submit" value="Submit">&nbsp;
+        <a href="index.php" class="btn btn-primary" role="button">Cancel</a>
+      </form>
+    </div>
+  </div>
+</div>
+<?php include("includes/scripts.php.php"); ?>
 
 </html>
